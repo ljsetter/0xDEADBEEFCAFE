fd04ffb51a3f050cbfdd72bc15c5fe52594bd3bc
Mason Wilie <masonwilie@gmail.com>
2019-09-30 13:16:02 -0600

    Trying to run on gpu

--- python/pneumonia/main.py
+++ python/pneumonia/main.py
@@ -7,13 +7,13 @@ from keras.preprocessing.image import ImageDataGenerator
 
 model = Sequential()
 
-model.add(Conv2D(32, 3, input_shape=(64, 64, 3), activation='relu') )
-model.add(MaxPooling2D(pool_size=(2, 2)) )
-model.add(Conv2D(32, 3) )
-model.add(MaxPooling2D(pool_size=(2, 2)) )
-model.add(Flatten() )
-model.add(Dense(128, activation='relu') )
-model.add(Dense(1, activation='sigmoid') )
+model.add(Conv2D(32, 3, input_shape=(64, 64, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, 3))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
@@ -24,18 +24,20 @@ train_gen = ImageDataGenerator( rescale=1./255,
 
 test_gen = ImageDataGenerator(rescale= 1./255)
 
-train_set = train_gen.flow_from_directory(  'python/pneumonia/data/train/',
+train_set = train_gen.flow_from_directory(  '/home/x/Documents/school/machine_learning/projects/0xDEADBEEFCAFE/python/pneumonia/data/train',
                                             target_size= (64, 64),
                                             batch_size=32,
                                             class_mode='binary')
 
-test_set = test_gen.flow_from_directory(    'python/pneumonia/data/test/',
+test_set = test_gen.flow_from_directory(    '/home/x/Documents/school/machine_learning/projects/0xDEADBEEFCAFE/python/pneumonia/data/test',
                                             target_size=(64, 64),
                                             batch_size=32,
                                             class_mode='binary')
 
 model.fit_generator(train_set,
-                    samples_per_epoch=8000,
+                    samples_per_epoch=50,
                     nb_epoch=25,
                     validation_data=test_set,
-                    nb_val_samples=2000)
+                    nb_val_samples=50)
+
+model.save('model.h5')
