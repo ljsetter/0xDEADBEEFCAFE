e64f61975530da338298396cf1994f31092d09e6
Josh Schoep <joshuaschoep@gmail.com>
2019-11-18 14:37:09 -0700

    Progress into fetching and posting data for webapp

--- web/start.js
+++ web/start.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const spawn = require('child_process').spawn;
+const async = require('express-async-await');
+const fetch = require('node-fetch');
 
 const SERVICE_PORT = 4701;
 
@@ -10,12 +12,16 @@ server = http.createServer(app);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.sendFile(directory + '/public/index.html')
+    res.sendFile(directory + '/public/index.html');
 });
 
-app.get('/process-image', function(req, res){
+app.post('/api/process-image', async function(req, res){
     /* Image should be processed here */
-    res.send(":)");
+    res.json(":)");
 });
+
+app.get('/api/process-image', async function(req, res){
+    res.send(":(");
+})
 
 app.listen(SERVICE_PORT);
