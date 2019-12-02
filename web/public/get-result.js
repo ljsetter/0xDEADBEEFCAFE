c8ae95d0c599844ee2938841c3c5e232178891b3
Josh Schoep <joshuaschoep@gmail.com>
2019-12-02 08:04:20 -0700

    Cleaned up web code

--- web/public/get-result.js
+++ web/public/get-result.js
@@ -1,18 +1,19 @@
-function postResults(data){
-    console.log(data);
+function postResults(data, clone){
     var content = document.getElementById("results-row").content;
     var clone = document.importNode(content, true);
+    file = document.getElementById("photo").files[0].name
+    clone.querySelector("h3").textContent += file
     data.json().then(body => {
         attributes = body.split(' ')
         path = attributes[0].replace('public', '');
         clone.querySelector("img").src = path
         clone.querySelector("h4").textContent += attributes[1];
-        document.getElementById("result-block").appendChild(clone);
+        document.getElementById("result-block").prepend(clone);
 
     })
 }
 
-function onUpload(){
+function onUpload(){    
     fetch("/api/process-image", {
         body: new FormData(document.getElementById("upload-form")),
         method: "post"
